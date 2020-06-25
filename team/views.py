015641741c6b3fbccea88e7cbe8457df7cc00c1d
Yao Josué <jyao2000@yahoo.fr>
2020-06-25 07:15:59 +0000

    team view

--- team/views.py
+++ team/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+# M : Schéma de donnée
+# V : Manipule les donnée
+# T : Display logic
+
+def home_page(request):
+    data = {
+    }
+    return render(request, 'index.html', data)
