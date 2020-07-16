1b78c933bf6fc2d15774bf4a382c20fc3a05a3d6
Yao Josué <jyao2000@yahoo.fr>
2020-07-16 22:43:33 +0000

    admin article

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = (
+        'auteur',
+        'titre',
+        'description',
+        'categorie',
+        'date_pub',
+    )
+    list_filter = (
+        'auteur',
+        'categorie',
+        'tags',
+    )
+    list_per_page = 50
+    search_fields = (
+        'titre',
+        'description'
+    )
+    date_hierarchy = 'date_pub'
+    fieldsets = [
+        (
+            'Visibles', {
+                'fields': [
+                    'auteur',
+                    'titre',
+                    'description',
+                    'date_pub'
+                ]
+            }
+        ),
+        (
+            'Image', {
+                'fields': [
+                    'cover'
+                ]
+            }
+        ),
+        (
+            'Contenu principal', {
+                'fields': [
+                    'contenu',
+                    'categorie',
+                    'tags'
+                ]
+            }
+        )
+    ]
+
+
+admin.site.register(models.Article, ArticleAdmin)
