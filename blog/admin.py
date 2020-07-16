0c3c746859dfccfe8dc41d242fd4f69852fd248b
Yao Josué <jyao2000@yahoo.fr>
2020-07-16 22:58:41 +0000

    admin standard

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,116 @@ from . import models
 
 
 # Register your models here.
+class CategorieAdmin(admin.ModelAdmin):
+    list_display = (
+        'libelle',
+        'status',
+        'date_add',
+        'date_upd'
+    )
+    list_filter = (
+        'status',
+    )
+    list_per_page = 50
+    search_fields = (
+        'libelle',
+    )
+    date_hierarchy = 'date_add'
+    fieldsets = [
+        (
+            'Info', {
+                'fields': [
+                    'libelle',
+                ]
+            }
+        ),
+        (
+            'Status et Activations', {
+                'fields': [
+                    'status',
+                ]
+            }
+        ),
+    ]
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = (
+        'libelle',
+        'status',
+        'date_add',
+        'date_upd'
+    )
+    list_filter = (
+        'status',
+    )
+    list_per_page = 50
+    search_fields = (
+        'libelle',
+    )
+    date_hierarchy = 'date_add'
+    fieldsets = [
+        (
+            'Info', {
+                'fields': [
+                    'libelle',
+                ]
+            }
+        ),
+        (
+            'Status et Activations', {
+                'fields': [
+                    'status',
+                ]
+            }
+        ),
+    ]
+
+
+
+class CommentaireAdmin(admin.ModelAdmin):
+    list_display = (
+        'nom',
+        'article',
+        'email',
+        'site',
+        'contenu',
+        'status',
+        'date_add',
+        'date_upd'
+    )
+    list_filter = (
+        'status',
+        'article',
+    )
+    list_per_page = 50
+    search_fields = (
+        'nom',
+        'contenu'
+    )
+    date_hierarchy = 'date_add'
+    fieldsets = [
+        (
+            'Info', {
+                'fields': [
+                    'article',
+                    'nom',
+                    'email',
+                    'site',
+                    'contenu'
+                ]
+            }
+        ),
+        (
+            'Status et Activations', {
+                'fields': [
+                    'status',
+                ]
+            }
+        ),
+    ]
+
+
 class ArticleAdmin(admin.ModelAdmin):
     list_display = (
         'auteur',
@@ -57,3 +167,6 @@ def _register(model, admin_class):
 
 
 _register(models.Article, ArticleAdmin)
+_register(models.Categorie, CategorieAdmin)
+_register(models.Tag, TagAdmin)
+_register(models.Commentaire, CommentaireAdmin)
