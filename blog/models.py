c5dc6549594a1e7546de1f46274f2637f7a7315a
Yao Josué <jyao2000@yahoo.fr>
2020-07-16 18:51:47 +0000

    model categorie create

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Categorie(models.Model):
+    libelle = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = "Categorie"
+        verbose_name_plural = "Categories"
