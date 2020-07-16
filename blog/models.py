7df152d6d763c2a3334c935bf0cba40069501185
Yao Josué <jyao2000@yahoo.fr>
2020-07-16 18:53:07 +0000

    model tag create

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Categorie(models.Model):
     libelle = models.CharField(max_length=255)
@@ -7,3 +8,12 @@ class Categorie(models.Model):
     class Meta:
         verbose_name = "Categorie"
         verbose_name_plural = "Categories"
+
+
+
+class Tag(models.Model):
+    libelle = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = "Tag"
+        verbose_name_plural = "Tags"
