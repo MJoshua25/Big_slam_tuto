80bf974a00fdae7db401b152f3ecd78f2e5997e8
Yao Josué <jyao2000@yahoo.fr>
2020-07-18 12:02:32 +0000

    affichage de données

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from . import models
 
 
 # M : Schéma de donnée
@@ -7,7 +8,9 @@ from django.shortcuts import render
 # T : Display logic
 
 def home_page(request):
+    articles = models.Article.objects.all()
     data = {
+        'articles': articles
     }
     return render(request, 'pages/blog/index.html', data)
 
